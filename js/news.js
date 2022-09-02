ba5f5a7278b07dc0aec716dc7be1ce4b9a16aa86
anowarzz <anowarhosen444@gmail.com>
2022-09-03 01:02:19 +0600

    categories disigned and responsive

--- js/news.js
+++ js/news.js
@@ -9,14 +9,18 @@ const loadCategories = () =>{
 
 
 const displayCategories = (categories) =>{
-    
+    const categoryContainer = document.getElementById("category-container");
+
+    categories.forEach( (category) =>{
+
+    const categoryDiv = document.createElement('Div');
+    categoryDiv.innerHTML = `
+    <a href = "" class="btn bg-secondary text-white link- fs-5 btn bg-seconday px-3 border border-dark">${category.category_name}</a>
+    `;
+    categoryContainer.appendChild(categoryDiv)
+    })
     
 }
-
-
-
-
-
 
 
 
