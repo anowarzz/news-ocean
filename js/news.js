77f41d1b7b916a71ff17c0c01d7d69c5e0c7c75e
anowarzz <anowarhosen444@gmail.com>
2022-09-03 08:29:35 +0600

    statice news displayed

--- js/news.js
+++ js/news.js
@@ -5,9 +5,10 @@ const loadCategories = () =>{
     fetch(url)
     .then(res => res.json())
     .then(data => displayCategories(data.data.news_category))
+    .catch(error => console.log(error))
 }
 
-
+// =========> Displaying Categories ==========>>
 const displayCategories = (categories) =>{
     const categoryContainer = document.getElementById("category-container");
 
@@ -15,13 +16,59 @@ const displayCategories = (categories) =>{
 
     const categoryDiv = document.createElement('Div');
     categoryDiv.innerHTML = `
-    <a href = "" class="btn bg-secondary text-white link- fs-5 btn bg-seconday px-3 border border-dark">${category.category_name}</a>
+    <button onclick="loadNews()" class="btn bg-secondary text-white link- fs-5 btn link-info">${category.category_name}</button>
+    
     `;
     categoryContainer.appendChild(categoryDiv)
     })
     
 }
 
-
-
 loadCategories();
+
+
+// ====>> Loadging News =========>>
+
+ const loadNews = () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/08`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displayNews(data.data))
+    .catch(error => console.log(error))
+ }
+
+
+ const displayNews = newsList =>{
+    
+    const newscontainer = document.getElementById("news-container");
+
+    newsList.forEach(news =>{
+        console.log(news);
+
+const newsDiv = document.createElement('div');
+
+newsDiv.innerHTML = `
+<div class="card my-4 border border-danger">
+  <div class="row">
+    <div class="col-md-4 border">
+      <img src="${news.thumbnail_url}" class=" w-100 rounded-start" alt="...">
+    </div>
+    <div class="col-md-7">
+      <div class="card-body">
+        <h5 class="card-title fs-3 fw-bold">${news.title}</h5>
+        <p class="card-text">${news.details}</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+newscontainer.appendChild(newsDiv);
+
+        
+    })
+ }
+
+
+ 
+loadNews();
